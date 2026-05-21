fadfc16587d28de123179f81e179e96847d135c2
Danish <danish.iszamri4070@gmail.com>
2026-05-21 17:50:17 +0800

    item and itemlost changes. added comment to code.

--- src/lostfound/model/Item.java
+++ src/lostfound/model/Item.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 public abstract class Item {
 
+    //Constants - fixed value that will be the same or never change
     public static final String STATUS_PENDING = "PENDING";
     public static final String STATUS_SUBMITTED = "SUBMITTED";
     public static final String STATUS_VERIFIED = "VERIFIED";
@@ -13,6 +14,8 @@ public abstract class Item {
     public static final String STATUS_CANCELLED = "CANCELLED";
     public static final String STATUS_ARCHIVED = "ARCHIVED";
 
+    //Encapsulation - variables that cannot be access directly outside the Item class
+    //In order to access it, other classes need to use getters and setters
     private String itemID;
     private String itemName;
     private String description;
@@ -31,7 +34,29 @@ public abstract class Item {
         this.status = status;
         this.submittedDate = LocalDate.now();
     }
+    
+    //Encapsulation - this method validate the status values, if the status is null then the values is pending.
+    public String validateStatus(String status){
+        if(status == null || status.trim().isEmpty()){
+            return STATUS_PENDING;
+        }
+        
+        String upperStatus = status.toUpperCase();
+        switch(upperStatus){
+            case STATUS_PENDING:
+            case STATUS_SUBMITTED:
+            case STATUS_VERIFIED:
+            case STATUS_MATCHED:
+            case STATUS_CLAIMED:
+            case STATUS_CANCELLED:
+            case STATUS_ARCHIVED:
+                return upperStatus;
+            default: 
+                return STATUS_PENDING;
+        }
+    }
 
+    //Getter Encapsulation - allow other classes to read the private data
     public String getItemID() {
         return itemID;
     }
@@ -60,6 +85,7 @@ public abstract class Item {
         return submittedDate;
     }
 
+    //Setter Encapsulation - Allow other classes to change the private data
     public void setItemName(String itemName) {
         if (itemName == null || itemName.trim().isEmpty()) {
             throw new IllegalArgumentException("Item name cannot be empty.");
@@ -83,11 +109,15 @@ public abstract class Item {
     }
 
     public void setStatus(String status) {
-        this.status = status;
+        this.status = validateStatus(status);
     }
 
+    //Polymorphism - force the child classes to create their own version of the trackStatus() method
+    //Same method name with different behaviour in each child class
     public abstract String trackStatus();
 
+    //Polymorphism - force the child classes to create their own version of the getStatusSummary() method
+    //Same method name with different behaviour in each child class
     public abstract String getStatusSummary();
 
     public boolean matchesKeyword(String keyword) {
--- src/lostfound/model/ItemLost.java
+++ src/lostfound/model/ItemLost.java
@@ -2,21 +2,26 @@ package lostfound.model;
 
 import java.time.LocalDate;
 
+//Inheritance - ItemLost class inherit to Item class
 public class ItemLost extends Item {
 
+    //Encapsulation - private and new fields that only ItemLost class have
     private String locationLost;
-    private LocalDate dateLost;
-    private String reporterID;
+    private LocalDate dateLost; //Composition - date that belongs to the item
+    private String reporterID; // Aggregation - ID that refers to a User
 
+    //Inheritance - calls the fields from parent(Item) class
     public ItemLost(String itemName, String description, Category category,
             String color, String locationLost, LocalDate dateLost,
             String reporterID) {
+        // super() calls the constructor in parent class which is Item class
         super(itemName, description, category, color, Item.STATUS_PENDING);
         this.locationLost = locationLost;
         this.dateLost = dateLost;
         this.reporterID = reporterID;
     }
 
+    //Getter Encapsulation - it let other classes to access the private data
     public String getLocationLost() {
         return locationLost;
     }
@@ -29,6 +34,7 @@ public class ItemLost extends Item {
         return reporterID;
     }
 
+    //Setter Encapsulation - let other classes to change the private fields or data
     public void setLocationLost(String locationLost) {
         if (locationLost == null || locationLost.trim().isEmpty()) {
             throw new IllegalArgumentException("Location lost cannot be empty.");
@@ -37,35 +43,52 @@ public class ItemLost extends Item {
     }
 
     public void setDateLost(LocalDate dateLost) {
-        if (dateLost == null || dateLost.isAfter(LocalDate.now())) {
+        if (dateLost == null) {
+            throw new IllegalArgumentException("Date lost cannot be null.");
+        }
+        if (dateLost.isAfter(LocalDate.now())) {
             throw new IllegalArgumentException("Date lost cannot be in the future.");
         }
         this.dateLost = dateLost;
     }
+    
+     public void setReporterID(String reporterID){
+        if (reporterID == null || reporterID.trim().isEmpty()){
+            throw new IllegalArgumentException("Reporter ID cannot be empty.");
+        }
+        this.reporterID = reporterID.trim();
+    }
 
+    //Polymorphism - item lost version of the parent class abstract 'trackStatus()' method
     @Override
     public String trackStatus() {
         switch (getStatus()) {
             case STATUS_PENDING:
                 return "Waiting for staff review.";
+            case STATUS_SUBMITTED:
+                return "Lost report submitted, awaiting verification.";
             case STATUS_VERIFIED:
                 return "Active lost report.";
             case STATUS_MATCHED:
                 return "Possible match found.";
             case STATUS_CLAIMED:
-                return "Recovered.";
+                return "Item has been recovered and claimed";
             case STATUS_CANCELLED:
-                return "Cancelled.";
+                return "Lost report has been cancelled.";
+            case STATUS_ARCHIVED:
+                return "Report is archived.";
             default:
                 return "Status: " + getStatus();
         }
     }
 
+    //Polymorphism - override the abstract parent to print specific lost report including locationLost, and reporterID attributes.
     @Override
     public String getStatusSummary() {
         return getItemID() + " | " + getItemName()
                 + " | Lost: " + locationLost
                 + " | Date: " + dateLost
+                + " | Reporter: " + reporterID
                 + " | Status: " + getStatus();
     }
 }
